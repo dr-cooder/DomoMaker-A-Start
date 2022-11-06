63aea37a234bb291c189775a0e722335d4856917
Felix Warner <fwarner2319@gmail.com>
2022-11-06 14:37:15 -0500

    Implemented middleware behavior

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -6,10 +6,10 @@ const makerPage = (req, res) => {
   Domo.findByOwner(req.session.account._id, (err, docs) => {
     if (err) {
       console.log(err);
-      return res.status(400).json({ error: 'An error has occurred! ' });
+      return res.status(400).json({ error: 'An error has occurred!' });
     }
 
-    return res.render('app', { domos: docs });
+    return res.render('app', { csrfToken: req.csrfToken(), domos: docs });
   });
 };
 
